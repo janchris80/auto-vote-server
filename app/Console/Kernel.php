dcbdaab3e127dc635e0b8048d4f4dc6f0b540a5f
iamjc93 <jan@goacquirely.com>
2023-12-01 10:43:26 +0800

    upgrade to laravel 10

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,23 +9,16 @@ class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        // $schedule->command('broadcast:vote')->everyMinute();
-        $schedule->command('broadcast:claim-rewards')->everyMinute();
     }
 
     /**
      * Register the commands for the application.
-     *
-     * @return void
      */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -18,16 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'uid',
-        'username',
+        'name',
         'email',
-        'enable',
-        'added',
-        'claim_reward',
-        'current_power',
-        'limit_power',
-        'sp',
-        'paused',
+        'password',
     ];
 
     /**
@@ -47,78 +40,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
-
-    public function trailers()
-    {
-        return $this->hasMany(Trailer::class, 'user_id');
-    }
-
-    public function curationTrailer()
-    {
-        return $this->hasOne(Trailer::class, 'user_id')
-            ->where('type', '=', 'curation');
-    }
-
-    public function downvoteTrailer()
-    {
-        return $this->hasOne(Trailer::class, 'user_id')
-            ->where('type', '=', 'downvote');
-    }
-
-    public function followers()
-    {
-        return $this->hasMany(Follower::class, 'user_id');
-    }
-
-    public function follower()
-    {
-        return $this->hasOne(Follower::class, 'user_id');
-    }
-
-    public function followersCount()
-    {
-        return $this->hasMany(Follower::class, 'user_id')
-            ->selectRaw('user_id, count(*) as count')
-            ->groupBy('user_id');
-    }
-
-    public function followings()
-    {
-        return $this->hasMany(Follower::class, 'follower_id');
-    }
-
-    public function followingsCount()
-    {
-        return $this->hasMany(Follower::class, 'follower_id')
-            ->selectRaw('follower_id, count(*) as count')
-            ->groupBy('follower_id');
-    }
-
-    public function followingsCurationCount()
-    {
-        return $this->hasMany(Follower::class, 'follower_id')
-            ->where('follower_type', '=', 'curation')
-            ->selectRaw('follower_id, count(*) as count')
-            ->groupBy('follower_id');
-    }
-
-    public function followingsDownvoteCount()
-    {
-        return $this->hasMany(Follower::class, 'follower_id')
-            ->where('follower_type', '=', 'downvote')
-            ->selectRaw('follower_id, count(*) as count')
-            ->groupBy('follower_id');
-    }
-
-    public function getIsFollowedByCurrentUserAttribute()
-    {
-        if (auth()->check()) {
-            return Follower::where('user_id', $this->id)
-                ->where('follower_id', auth()->id())
-                ->exists();
-        }
-
-        return false;
-    }
 }
