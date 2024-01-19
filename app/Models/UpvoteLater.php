d06c896ab0e1eefca1789cc27dd56ceddc32d2e1
iamjc93 <iamjc93@gmail.com>
2024-01-19 18:34:29 +0800

    fix: upvote model uuid

--- app/Models/UpvoteLater.php
+++ app/Models/UpvoteLater.php
@@ -4,16 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class UpvoteLater extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'uuid',
         'voter',
         'author',
         'permlink',
         'weight',
         'time_to_vote',
     ];
+
+    // Set default values for attributes
+    protected $attributes = [
+        'uuid' => null,  // Use null or provide a default UUID here
+    ];
+
+    // If you want to generate a UUID automatically when creating a new instance
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->uuid = $model->uuid ?? Str::uuid();
+        });
+    }
 }
